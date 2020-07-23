838db2fb9dc69089a5ccaaf9034892d6a0aa3b1e
kashua14 <31764137+kashua14@users.noreply.github.com>
2020-07-23 14:47:22 +0300

    Initial commit

---
+++ app/src/main/java/com/jambula/notifications/services/MyFirebaseMessagingService.java
@@ -0,0 +1,65 @@
+package com.jambula.notifications.services;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.jambula.notifications.R;
+
+import java.util.Random;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private NotificationManager notificationManager;
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        //Setting up Notification channels for android O and above
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            setupChannels();
+        }
+        int notificationId = new Random().nextInt(60000);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, ADMIN_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_notification_small)  //a resource for your custom small icon
+                .setContentTitle(remoteMessage.getData().get("title")) //the "title" value you sent in your notification
+                .setContentText(remoteMessage.getData().get("message")) //ditto
+                .setAutoCancel(true)  //dismisses the notification on click
+                .setSound(defaultSoundUri);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(notificationId /* ID of notification */, notificationBuilder.build());
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void setupChannels(){
+        CharSequence adminChannelName = getString(R.string.notifications_admin_channel_name);
+        String adminChannelDescription = getString(R.string.notifications_admin_channel_description);
+
+        NotificationChannel adminChannel;
+        adminChannel = new NotificationChannel(ADMIN_CHANNEL_ID, adminChannelName, NotificationManager.IMPORTANCE_LOW);
+        adminChannel.setDescription(adminChannelDescription);
+        adminChannel.enableLights(true);
+        adminChannel.setLightColor(Color.RED);
+        adminChannel.enableVibration(true);
+        if (notificationManager != null) {
+            notificationManager.createNotificationChannel(adminChannel);
+        }
+    }
+}
